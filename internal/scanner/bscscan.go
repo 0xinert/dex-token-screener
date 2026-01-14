b1fe40a22280d5ce910b2d52122b2bc031f12eca
Aryan Pandey <aryan.pandey@mudrex.com>
2026-01-14 17:48:57 +0530

    implemented the funtion to calculate a token's top 10 holder concentration

--- internal/scanner/bscscan.go
+++ internal/scanner/bscscan.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/notlelouch/go-interview-practice/DEX-Token-Screener/internal/models"
@@ -40,9 +41,9 @@ func NewBscScanClient(apiKey string) *BscScanClient {
 	}
 }
 
-func (c *BscScanClient) IsContractVerified(address string) (bool, error) {
+func (c *BscScanClient) IsContractVerified(contractAddress string) (bool, error) {
 	url := fmt.Sprintf("%s?address=%s&chainId=56&module=contract&apikey=%s&action=getsourcecode",
-		c.baseURL, address, c.apikey)
+		c.baseURL, contractAddress, c.apikey)
 
 	resp, err := c.httpClient.Get(url)
 	if err != nil {
@@ -61,4 +62,56 @@ func (c *BscScanClient) IsContractVerified(address string) (bool, error) {
 	}
 
 	return result.Status == "1" && len(result.Result) > 0 && result.Result[0].SourceCode != "", nil
+}
+
+func (c *BscScanClient) GetTop10HoldersConcentration(tokenAddress string) (float64, error) {
+	url := fmt.Sprintf("%s?&chainId=56&module=token&action=topholders&contractaddress=%s&offet=10&apikey=%s",
+		c.baseURL, tokenAddress, c.apikey)
+
+	resp, err := c.httpClient.Get(url)
+	if err != nil {
+		fmt.Println("Error fetching token holders:", err)
+		return 0, err
+	}
+
+	defer resp.Body.Close()
+
+	body, _ := io.ReadAll(resp.Body)
+
+	var result TokenHoldersResponse
+	if err := json.Unmarshal(body, &result); err != nil {
+		fmt.Println("Error unmarshaling response:", err)
+		return 0, err
+	}
+
+	if len(result.Result) == 0 {
+		return 0, nil
+	}
+
+	var totalTop10Balance float64
+	for _, holder := range result.Result {
+		balance, err := strconv.ParseFloat(holder.Balance, 64)
+		if err != nil {
+			fmt.Println("Error parsing top 10 holder balance:", err)
+		}
+		totalTop10Balance += balance
+	}
+
+	totalsupply, err := c.GetTotalSupply(tokenAddress)
+	if err != nil {
+		fmt.Println("Error fetching total supply:", err)
+		return 0, err
+	}
+
+	if totalsupply == 0 {
+		return 0, nil
+	}
+
+	return (totalTop10Balance / totalsupply) * 100, nil
+
+	return 0, nil
+}
+
+func (c *BscScanClient) GetTotalSupply(tokenAddress string) (float64, error) {
+	return 0, nil
 }
