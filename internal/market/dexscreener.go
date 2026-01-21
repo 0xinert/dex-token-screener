8fa13bb76e156ac13390cd81407c897c816016f0
Aryan Pandey <aryan.pandey@mudrex.com>
2026-01-21 16:54:14 +0530

    implementation of the dexscrener client, that will return us the liq,vol,age,fragmentation details for a given token pair

--- internal/market/dexscreener.go
+++ internal/market/dexscreener.go
@@ -22,18 +22,18 @@ type DexScreenerResponse struct {
 
 func NewDexScreenerClient() *DexScreenerClient {
 	return &DexScreenerClient{
-		baseURL:    "https://api.dexscreener.com/latest/dex/tokens",
+		baseURL:    "https://api.dexscreener.com",
 		httpClient: &http.Client{Timeout: 10 * time.Second},
 	}
 }
 
-// GetLiquidityMetrics fetches liquidity and volume data
-func (d *DexScreenerClient) GetLiquidityMetrics(address string) (liquidity, volume float64, err error) {
-	url := fmt.Sprintf("%s/%s", d.baseURL, address)
+// GetPairMetrics fetches liquidity and volume data
+func (d *DexScreenerClient) GetPairMetrics(address string) (liquidity, volume float64, fragmentationSafe bool, largetsSingleLiquidityPoolAgeDays float64, err error) {
+	url := fmt.Sprintf("%s/token-pairs/v1/bsc/%s", d.baseURL, address)
 
 	resp, err := d.httpClient.Get(url)
 	if err != nil {
-		return 0, 0, err
+		return 0, 0, false, 0, err
 	}
 	defer resp.Body.Close()
 
@@ -41,18 +41,37 @@ func (d *DexScreenerClient) GetLiquidityMetrics(address string) (liquidity, volu
 
 	var result DexScreenerResponse
 	if err := json.Unmarshal(body, &result); err != nil {
-		return 0, 0, err
+		return 0, 0, false, 0, err
 	}
 
 	if len(result.Pairs) == 0 {
-		return 0, 0, fmt.Errorf("no pairs found")
+		return 0, 0, false, 0, fmt.Errorf("no DEXScreener pairs found for token %s", address)
 	}
 
+	// Filter to keep only USDT pairs
+	usdtPairs := make([]models.DexScreenerPair, 0)
+	for _, pair := range result.Pairs {
+		if pair.QuoteToken.Address == "0x55d398326f99059ff775485246999027b3197955" {
+			usdtPairs = append(usdtPairs, pair)
+		}
+	}
+	result.Pairs = usdtPairs
+
+	largetsSingleLiquidityPool := models.DexScreenerPair{}
 	// Aggregate liquidity and volume across all pairs
 	for _, pair := range result.Pairs {
+		if pair.Liquidity.USD > largetsSingleLiquidityPool.Liquidity.USD {
+			largetsSingleLiquidityPool = pair
+		}
 		liquidity += pair.Liquidity.USD
 		volume += pair.Volume.H24
 	}
 
-	return liquidity, volume, nil
+	if largetsSingleLiquidityPool.Liquidity.USD >= 0.5*liquidity {
+		fragmentationSafe = true
+	}
+	// Age of largets single liquidity pool in days
+	largetsSingleLiquidityPoolAgeDays = time.Since(time.Unix(largetsSingleLiquidityPool.PairCreatedAt, 0)).Hours() / 24
+
+	return liquidity, volume, fragmentationSafe, largetsSingleLiquidityPoolAgeDays, nil
 }
